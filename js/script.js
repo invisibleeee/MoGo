988d2a9b01eab49ddbc2272c6aad3ee1967f6544
invisibleeee <47563011+invisibleeee@users.noreply.github.com>
2022-11-15 16:23:22 +0300

    upd. 15.11.22

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.spoiler__title').click(function(event) {
         if ($('.service-2__block').hasClass('one')) {
             $('.spoiler__title').not($(this)).removeClass('active');
-            $('.spoiler__text').not($(this)).slideUp(300);
+            $('.spoiler__text').not($(this).next()).slideUp(300);
         }
         $(this).toggleClass('active').next().slideToggle(300);
     });
